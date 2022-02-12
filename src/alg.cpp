16a66b4891dacf34d2404718289436aaa991ba2b
Смирнова Алёна <99176199+aleonawith@users.noreply.github.com>
2022-02-12 13:18:12 +0300

    Update alg.cpp
    Смирнова Алёна 21-ИСТ-2

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,10 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    // поместить сюда текст реализации алгоритма (на С++)
+    if (a > b)
+        return gcd(a - b, b);
+    else if (a < b)
+        return gcd(a, b - a);
+    else if (a == 0 || b == 0)
+        return a + b;
 }
